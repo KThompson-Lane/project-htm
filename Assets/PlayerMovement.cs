42c8266835ef000128584abd7b1719982360c417
A-Russell23 <avrrussell@gmail.com>
2023-02-12 10:34:52 +0000

    Follows mouse

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,18 +7,27 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-
+    public Camera cam;
+    
     Vector2 movement;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        //convert mouse position from screen co-ords to world units
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
     
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f; //z rotation. Note: Atan2 takes y first, then x
+        rb.rotation = angle;
     }
 }
