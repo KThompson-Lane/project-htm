41ceba62fd8b3b918f9d1dc8c8ba19202159b7fa
A-Russell23 <avrrussell@gmail.com>
2023-02-12 11:34:42 +0000

    Ensured bullets destroyed on collision/ after set time

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -7,6 +7,12 @@ public class Bullet : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D col)
     {
-        //Destroy(gameObject);
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        //Ensure bullet is destroyed after a set time so they don't linger
+        Destroy(gameObject, 10f);
     }
 }
