84449f251abaf8b8e60e3d33e73bd6b36edeb3e2
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-03-10 15:10:54 +0000

    add boss room

--- Assets/Code/DungeonGeneration/DungeonFloorScriptableObject.cs
+++ Assets/Code/DungeonGeneration/DungeonFloorScriptableObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -17,44 +18,73 @@ public class DungeonFloorScriptableObject : ScriptableObject
     //  top left cell is 0,1, bottom right is 4,5
     public Vector2Int floorSize = new(7, 7);
     public int level;
+    public int rooms;
     public DungeonRoomScriptableObject StartRoom;
     public List<DungeonRoomScriptableObject> BasicRooms;
     public List<DungeonRoomScriptableObject> BossRooms;
-    public Dictionary<int, DungeonRoomScriptableObject> floorplan = new();
-    public Queue<int> Cells = new Queue<int>();
-    public int rooms;
-    public bool addedNeighbour = false;
+    
+    public Dictionary<int, DungeonRoomScriptableObject> floorplan;
+    private Queue<int> Cells;
+    
+    private bool addedNeighbour = false;
+    private System.Random _random;
     public void GenerateFloor()
     {
+        _random = new System.Random();
+        bool validFloor = false;
+        while (!validFloor)
+            validFloor = CreateFloorPlan();
+    }
+
+    private bool CreateFloorPlan()
+    {
+        floorplan = new();
+        Cells = new();
+        
         //  Determine number of rooms
         //  TODO: Change formula
         rooms = Random.Range(1, 3) + 5 +(int) (level * 2.6);
         List<int> endRooms = new List<int>();
         
         //  Place our starting cell (2,3 (middle)) into the queue
-        int startCoordinate = ((floorSize.y / 2) * 10) + (floorSize.y / 2);
-        floorplan.Add(startCoordinate, StartRoom);
-        Cells.Enqueue(startCoordinate);
+        int startCell = ((floorSize.y / 2) * 10) + (floorSize.y / 2);
+        floorplan.Add(startCell, StartRoom);
+        Cells.Enqueue(startCell);
+        
+        //  Go over all cells adding neighbours
         while (Cells.Count > 0)
         {
             //  Check neighbouring cells
             int currentCell = Cells.Peek();
             addedNeighbour = false;
             //  Top neighbour
-            CheckConditions(currentCell - 10);
+            CheckNeighbourConditions(currentCell - 10);
             //  Bottom neighbour
-            CheckConditions(currentCell + 10);
+            CheckNeighbourConditions(currentCell + 10);
             //  Right neighbour
-            CheckConditions(currentCell + 1);
+            CheckNeighbourConditions(currentCell + 1);
             //  Left neighbour
-            CheckConditions(currentCell - 1);
+            CheckNeighbourConditions(currentCell - 1);
             if(!addedNeighbour)
                 endRooms.Add(currentCell);
             Cells.Dequeue();
         }
-    }
 
-    private void CheckConditions(int cell)
+        //  Ensure we made a valid floor
+        //TODO: MAKE THIS NOT ICKY
+        //  Find our boss room and make sure it isn't neighbouring with start room
+        int bossCell = endRooms.Last();
+        if (floorplan.Count != rooms) return false;
+        if (startCell + 10 == bossCell) return false;
+        if (startCell - 10 == bossCell) return false;
+        if (startCell + 1 == bossCell) return false;
+        if (startCell - 1 == bossCell) return false;
+        
+        var bossRoom = BossRooms[_random.Next(BossRooms.Count)];
+        floorplan[bossCell] = bossRoom;
+        return true;
+    }
+    private void CheckNeighbourConditions(int cell)
     {
         if (floorplan.ContainsKey(cell))
             return;
