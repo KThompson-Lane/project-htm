1fd1f9acccc73bb0da0b0c74f2b9728a6e3cb17a
A-Russell23 <avrrussell@gmail.com>
2023-03-17 13:30:42 +0000

    Reset on death

--- Assets/Code/Player/HealthManager.cs
+++ Assets/Code/Player/HealthManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu]
 public class HealthManager : ScriptableObject //todo - probably rename this to be PlayerHealthManager
@@ -26,5 +27,13 @@ public class HealthManager : ScriptableObject //todo - probably rename this to b
         health -= amount;
         // Trigger healthChangedEvent
         HealthChangedEvent.Invoke(health);
+        
+        if (health <= 0)
+        {
+            //reset game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); //todo - change this to first level
+            health = maxHealth;
+            HealthChangedEvent.Invoke(health);
+        }
     }
 }
