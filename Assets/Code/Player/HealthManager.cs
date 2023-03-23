dfabe498264aa311a0673ef3b85c306820bcb840
A-Russell23 <avrrussell@gmail.com>
2023-03-23 11:53:53 +0000

    Quick tidy up

--- Assets/Code/Player/HealthManager.cs
+++ Assets/Code/Player/HealthManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 [CreateAssetMenu]
-public class HealthManager : ScriptableObject //todo - probably rename this to be PlayerHealthManager
+public class HealthManager : ScriptableObject //todo - probably rename this to be PlayerHealthManagerSO
 {
     public float health = 3;
     
@@ -27,13 +27,11 @@ public class HealthManager : ScriptableObject //todo - probably rename this to b
         health -= amount;
         // Trigger healthChangedEvent
         HealthChangedEvent.Invoke(health);
-        
-        if (health <= 0)
-        {
-            //reset game
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); //todo - change this to first level
-            health = maxHealth;
-            HealthChangedEvent.Invoke(health);
-        }
+
+        if (!(health <= 0)) return;
+        //reset game
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //todo - change this to first level
+        health = maxHealth;
+        HealthChangedEvent.Invoke(health);
     }
 }
