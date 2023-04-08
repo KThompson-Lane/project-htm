40a35fc1c4ec3afa87f3038f0b8ed9807b8b4a72
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-04-08 13:36:38 +0100

    Added new player and weapon sprite and animations

--- Assets/Code/Player/Shooting.cs
+++ Assets/Code/Player/Shooting.cs
@@ -17,10 +17,12 @@ public class Shooting : MonoBehaviour
     private float _interval; // time between shots
     
     private bool _isShooting;
+    [SerializeField] private Animator _gunAnimator;
 
     public void Start()
     {
         _interval = 60 / rateOfFire; //1 second / rate of fire
+        _gunAnimator.SetFloat("Fire Rate", _interval);
     }
 
     public void OnShoot(InputAction.CallbackContext context)
@@ -33,7 +35,7 @@ public class Shooting : MonoBehaviour
     {
         //Fire when left mouse held
         if(_isShooting)
-        { ;
+        {
             if (_coolDown <= 0f)
             {
                 Shoot();
@@ -54,5 +56,6 @@ public class Shooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        _gunAnimator.SetTrigger("Shoot");
     }
 }
