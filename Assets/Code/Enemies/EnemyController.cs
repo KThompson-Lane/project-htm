d2bc833967e83667157a2736c9ab31d0e89795e5
A-Russell23 <avrrussell@gmail.com>
2023-04-09 12:18:49 +0100

    Added sprites and tidied EnemySO

--- Assets/Code/Enemies/EnemyController.cs
+++ Assets/Code/Enemies/EnemyController.cs
@@ -11,26 +11,31 @@ public class EnemyController : MonoBehaviour
     private EnemyAttackSO _enemyAttackSo;
 
     private float _currentHealth;
-    
-    [SerializeField] 
-    private float RotationSpeed = 3.0f; //todo - might want to move to SO, might want to have this fixed if it seems right, or maybe a func of move speed
-    
+
     private float _moveSpeed;
+    private float _rotationSpeed;
     private float _range;
     
     private Rigidbody2D _mRb2d;
     private Transform _mPlayerTransform; // for look at location
+
+    private Sprite _sprite;
 
     // Start is called before the first frame update
     private void Awake()
     {
         _enemyAttackSo = enemySO.enemyAttackType;
 
+        // sprite
+        _sprite = enemySO.enemySprite;
+        GetComponent<SpriteRenderer>().sprite = _sprite;
+
         // health 
         _currentHealth = enemySO.GetMaxHealth();
         
         // movement
         _moveSpeed = enemySO.GetSpeed();
+        _rotationSpeed = enemySO.GetRotationSpeed();
         _range = _enemyAttackSo.range;
         _mRb2d = GetComponent<Rigidbody2D>();
         _mPlayerTransform = GameObject.FindWithTag("Player").transform; //todo - might want to change if not all enemies follow player
@@ -52,7 +57,7 @@ public class EnemyController : MonoBehaviour
         // rotate to look at player
         var angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90; //z rotation. Note: Atan2 takes y first, then x, subtract 90degrees for sprite rotation
         var rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation =  Quaternion.Slerp(transform.rotation, rotation, Time.fixedDeltaTime * RotationSpeed);
+        transform.rotation =  Quaternion.Slerp(transform.rotation, rotation, Time.fixedDeltaTime * _rotationSpeed);
     }
 
     public virtual void TakeDamage(int damage)
