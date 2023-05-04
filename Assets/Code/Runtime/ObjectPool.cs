5cc8e90fff850e9b6cedff23614f3e53b2546abb
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-05-04 14:21:35 +0100

    implement an object pool

---
+++ Assets/Code/Runtime/ObjectPool.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject poolObject;
+    [SerializeField] private int poolAmount = 20;
+
+    public static ObjectPool instance;
+    
+    private List<GameObject> pooledObjects = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < poolAmount; i++)
+        {
+            var obj = Instantiate(poolObject, transform);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject RetrieveFromPool()
+    {
+        return pooledObjects.FirstOrDefault(t => !t.activeInHierarchy);
+    }
+    
+}
--- Assets/Code/Runtime/Player/Bullet.cs
+++ Assets/Code/Runtime/Player/Bullet.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -13,15 +13,20 @@ public class Bullet : MonoBehaviour
             enemyController.TakeDamage(1); //todo - variable damage
         }
         
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 
     private void Start()
     {
         //Ensure bullet is destroyed after a set time so they don't linger
-        Destroy(gameObject, 10f);
+        StartCoroutine(DisableAfter(10));
     }
-    
+
+    public IEnumerator DisableAfter(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        gameObject.SetActive(false);
+    }
     public void SetDamage(float damage)
     {
         Damage = damage;
