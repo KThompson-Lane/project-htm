43d3ee1b3eda452f7a17c2e0005ab86e8ad4ae66
KThompson-Lane <57400783+KThompson-Lane@users.noreply.github.com>
2023-05-16 11:06:05 +0100

    fix enemy spawn locations

--- Assets/Code/Runtime/DungeonGeneration/ScriptableObjects/NormalRoomScriptableObject.cs
+++ Assets/Code/Runtime/DungeonGeneration/ScriptableObjects/NormalRoomScriptableObject.cs
@@ -40,8 +40,8 @@ namespace Code.DungeonGeneration
             var possibleLocations = layout.FloorTiles.Tiles.Where(x => (x.Tile is not RuleTile)).SelectMany(x => x.Positions);
             possibleLocations =
                 possibleLocations.Where(position =>
-                    (position.x != layout.RoomBounds.xMax && position.x != layout.RoomBounds.xMin) &&
-                    (position.y != (layout.RoomBounds.yMax-1) && position.y != layout.RoomBounds.yMin));
+                    (position.x < layout.RoomBounds.xMax -3 && position.x > layout.RoomBounds.xMin + 3) &&
+                    (position.y < (layout.RoomBounds.yMax -2) && position.y > layout.RoomBounds.yMin + 2));
             
             foreach (var enemyPosition in possibleLocations.OrderBy(_ => Guid.NewGuid()))
             {
